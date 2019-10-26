1c67030f98caf00f0af97907b811fce80c597e46
mpuchyr <matt.puchyr@gmail.com>
2019-10-26 15:26:21 -0500

    Add initial login and signup functionality to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,39 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    user = User.new(params)
+    if user.save
+      redirect :login
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      puts "Hello"
+      redirect '/'
+    else
+      redirect '/login'
+    end
+  end
+
+
+
 
   helpers do
     def logged_in?
