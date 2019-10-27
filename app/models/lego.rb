a80dd74336746d538e6448d5650011cd8ebb0e0e
mpuchyr <matt.puchyr@gmail.com>
2019-10-27 15:01:35 -0500

    Add validations to User and Lego classes

--- app/models/lego.rb
+++ app/models/lego.rb
@@ -1,3 +1,8 @@
 class Lego < ActiveRecord::Base
     belongs_to :user
+
+    validates :name, presence: true
+    validates :theme, presence: true
+    validates :num_of_pieces, numericality: { only_integer: true}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :legos
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, presence: true
 end
