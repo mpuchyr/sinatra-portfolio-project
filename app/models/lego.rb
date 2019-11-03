415645a4a34da12315f2257c9bb1239cbca0b4fe
mpuchyr <matt.puchyr@gmail.com>
2019-11-03 13:21:17 -0600

    Remove validation for Lego theme because it causes an error

--- app/models/lego.rb
+++ app/models/lego.rb
@@ -3,7 +3,6 @@ class Lego < ActiveRecord::Base
     belongs_to :theme
 
     validates :name, presence: true
-    validates :theme_id, presence: true
     validates :num_of_pieces, numericality: { only_integer: true }
 
 end
